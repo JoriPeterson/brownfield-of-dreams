5efda0ace488207f53cad30dcd832a524707d460
Jake Miller <jake0miller@gmail.com>
2019-08-26 15:48:22 -0600

    Add sad path test for new tutorial

--- spec/features/admin/admin_can_create_tutorials_spec.rb
+++ spec/features/admin/admin_can_create_tutorials_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'An admin user can add tutorials' do
-  it 'clicks on add tutoral' do
+  before :each do
     admin = create(:user, role: 1)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -11,7 +11,9 @@ describe 'An admin user can add tutorials' do
     click_on 'New Tutorial'
 
     expect(current_path).to eq(new_admin_tutorial_path)
+  end
 
+  it 'clicks on add tutoral' do
     fill_in 'tutorial[title]', with: "How to Active Record"
     fill_in 'tutorial[description]', with: "Like a boss"
     fill_in 'tutorial[thumbnail]', with: "https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg"
@@ -20,5 +22,15 @@ describe 'An admin user can add tutorials' do
 
     expect(page).to have_content("Successfully created tutorial.")
     expect(current_path).to eq(new_admin_tutorial_video_path(Tutorial.last))
+  end
+
+  it 'cannot omit information' do
+    fill_in 'tutorial[title]', with: "How to Active Record"
+    fill_in 'tutorial[thumbnail]', with: "https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg"
+
+    click_on "Save"
+
+    expect(page).to have_content("Unable to create tutorial.")
+    expect(current_path).to eq(new_admin_tutorial_path)
   end
 end
