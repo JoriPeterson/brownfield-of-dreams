82b663398cb3c79e9f6585ddb08012a0e15959d2
Jori Peterson <joripeterson@gmail.com>
2019-08-26 13:32:54 -0600

    Add feature test and logic for classroom content

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,4 +1,13 @@
 class TutorialsController < ApplicationController
+
+	def index
+		if current_user
+			@tutorials = Tutorial.all
+		else
+			@tutorials = Tutorial.where("classroom = ?", "false")
+		end
+	end
+
   def show
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
