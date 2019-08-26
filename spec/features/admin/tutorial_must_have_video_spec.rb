491285b984dfea3468de2eb5ca2d1ef9d214fef1
Jori Peterson <joripeterson@gmail.com>
2019-08-26 15:43:55 -0600

    Add new action and form for admin videos"

--- spec/features/admin/tutorial_must_have_video_spec.rb
+++ spec/features/admin/tutorial_must_have_video_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe "A tutorial must have a video" do
 	it "Or admin will be rerouted to add video" do
-	  let(:tutorial) { create(:tutorial) }
-	  let(:admin)    { create(:admin) }
+	  tutorial = create(:tutorial)
+	  admin   = create(:admin)
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
     visit new_admin_tutorial_path(tutorial)
 
-		# still in progress...
+
   end
 end
