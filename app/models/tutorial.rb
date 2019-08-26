6c3d192928dd850c1694cfb73d9df56ea2df10c7
Jori Peterson <joripeterson@gmail.com>
2019-08-26 12:52:50 -0600

    Update model and testing for tutorials

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,4 +2,10 @@ class Tutorial < ApplicationRecord
   has_many :videos, ->  { order(position: :ASC) }
   acts_as_taggable_on :tags, :tag_list
   accepts_nested_attributes_for :videos
+
+	validates_presence_of :title
+	validates_presence_of :description
+	validates_presence_of :thumbnail
+	validates_presence_of :playlist_id
+	validates_presence_of :classroom
 end
