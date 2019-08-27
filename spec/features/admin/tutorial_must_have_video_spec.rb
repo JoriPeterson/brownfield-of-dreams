b44c7dfae4af4cf9d6aff61b4f46b1ac0a91bcf1
Jori Peterson <joripeterson@gmail.com>
2019-08-26 18:13:14 -0600

    Update testing for tutorials having videos
    Co-Authored-By: Jake Miller <jake0miller@gmail.com>

--- spec/features/admin/tutorial_must_have_video_spec.rb
+++ spec/features/admin/tutorial_must_have_video_spec.rb
@@ -9,6 +9,19 @@ describe "A tutorial must have a video" do
 
     visit new_admin_tutorial_path(tutorial)
 
+		fill_in 'tutorial[title]', with: "How to Active Record"
+    fill_in 'tutorial[description]', with: "Like a boss"
+    fill_in 'tutorial[thumbnail]', with: "https://i.ytimg.com/vi/qMkRHW9zE1c/hqdefault.jpg"
 
+    click_on "Save"
+
+		expect(current_path).to eq(new_admin_tutorial_video_path(Tutorial.last))
+
+
+
+	# 	fill_in 'video[youtube_url]'
+	# 	fill_in 'video[video_id]'
+	#
+	# 	click_on "Submit"
   end
 end
