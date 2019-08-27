a5788a2c1ca707777a1e774eedc96cafda7d2a29
Jori Peterson <joripeterson@gmail.com>
2019-08-27 15:39:50 -0600

    Show active status and register messages on dashboard
    Co-Authored-By: Jake Miller <jake0miller@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 	has_many :friends, :through => :friendships, dependent: :destroy
 
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password
+  validates_presence_of :password_digest
   validates_presence_of :first_name
   enum role: [:default, :admin]
   has_secure_password
