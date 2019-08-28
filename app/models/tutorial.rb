96ab67c06561f907b47fdaade22af158f18560b9
Jake Miller <jake0miller@gmail.com>
2019-08-28 16:57:02 -0600

    Refactor tutorial active record queries into separate methods

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -6,51 +6,53 @@ class Tutorial < ApplicationRecord
 	validates_presence_of :title
 	validates_presence_of :description
 
-	def self.user_tutorials(current_user, tag, page)
-		if current_user.nil?
-			if tag
-				Tutorial.where("classroom = ?", "false")
-					.joins(:videos)
-					.select('tutorials.*')
-					.tagged_with(tag)
-					.paginate(:page => page, :per_page => 5)
-					.having('count(videos.*) > 0')
-					.group(['tutorials.id, videos.id'])
-					.distinct
-			else
-				Tutorial.where("classroom = ?", "false")
-					.joins(:videos)
-					.select('tutorials.*')
-					.paginate(:page => page, :per_page => 5)
-					.having('count(videos.*) > 0')
-					.group(['tutorials.id, videos.id'])
-					.distinct
-			end
-		elsif current_user.admin?
-			if tag
-				Tutorial.tagged_with(tag)
-					.paginate(:page => page, :per_page => 5)
-			else
-				Tutorial.all
-					.paginate(:page => page, :per_page => 5)
-			end
+  def self.visitor_tutorials(tag, page)
+  	if tag
+  		Tutorial.where("classroom = ?", "false")
+  			.joins(:videos)
+  			.select('tutorials.*')
+  			.tagged_with(tag)
+  			.paginate(:page => page, :per_page => 5)
+  			.having('count(videos.*) > 0')
+  			.group(['tutorials.id, videos.id'])
+  			.distinct
+  	else
+  		Tutorial.where("classroom = ?", "false")
+  			.joins(:videos)
+  			.select('tutorials.*')
+  			.paginate(:page => page, :per_page => 5)
+  			.having('count(videos.*) > 0')
+  			.group(['tutorials.id, videos.id'])
+  			.distinct
+  	end
+  end
+
+	def self.admin_tutorials(tag, page)
+		if tag
+			Tutorial.tagged_with(tag)
+				.paginate(:page => page, :per_page => 5)
 		else
-			if tag
-				Tutorial.joins(:videos)
-					.select('tutorials.*')
-					.tagged_with(tag)
-					.paginate(:page => page, :per_page => 5)
-					.having('count(videos.id) > 0')
-					.group(['tutorials.id, videos.id'])
-					.distinct
-			else
-				Tutorial.joins(:videos)
-					.select('tutorials.*')
-					.paginate(:page => page, :per_page => 5)
-					.having('count(videos.id) > 0')
-					.group(['tutorials.id, videos.id'])
-					.distinct
-			end
+			Tutorial.all
+				.paginate(:page => page, :per_page => 5)
 		end
 	end
+
+  def self.user_tutorials(tag, page)
+  	if tag
+  		Tutorial.joins(:videos)
+  			.select('tutorials.*')
+  			.tagged_with(tag)
+  			.paginate(:page => page, :per_page => 5)
+  			.having('count(videos.id) > 0')
+  			.group(['tutorials.id, videos.id'])
+  			.distinct
+  	else
+  		Tutorial.joins(:videos)
+  			.select('tutorials.*')
+  			.paginate(:page => page, :per_page => 5)
+  			.having('count(videos.id) > 0')
+  			.group(['tutorials.id, videos.id'])
+  			.distinct
+  	end
+  end
 end
