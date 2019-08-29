6d45b50187e56d7b60d854bc55c5e3aeaf74435f
Jake Miller <jake0miller@gmail.com>
2019-08-28 18:51:15 -0600

    Add sendgrid module

--- config/application.rb
+++ config/application.rb
@@ -31,3 +31,19 @@ module PersonalProject
     config.generators.system_tests = nil
   end
 end
+
+module YouveChangedV2
+  class Application < Rails::Application
+    config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.sendgrid.net',
+      port:                 '587',
+      domain:               'example.com',
+      user_name:            ENV["SENDGRID_USERNAME"],
+      password:             ENV["SENDGRID_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+    }
+  end
+end
